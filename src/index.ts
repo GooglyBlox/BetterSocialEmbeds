1314e6047c3e1ce29fc7852a945a257fcd9af9bf
GooglyBlox <cemcimyildiz@gmail.com>
2024-03-18 15:06:34 -0700

    Potential fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-import instagramPatch from "./patches/sendMessage";
+import { storage } from "@vendetta/plugin";
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+import Settings from "./ui/pages/Settings";
+
+const Messages = findByProps("sendMessage", "receiveMessage");
+
+let unpatch;
 
 export default {
-   onLoad: () => {
-      instagramPatch();
-   },
-   onUnload: () => {
-      // Add any necessary cleanup logic here
-   }
-};
+    onLoad: () => {
+        unpatch = before("sendMessage", Messages, (args) => {
+            const content = args[1].content as string;
+            const instagramRegex = /https:\/\/www\.instagram\.com\/(p\/|reel\/)[^ ]+/g;
+            if (instagramRegex.test(content)) {
+                args[1].content = content.replace(
+                    instagramRegex,
+                    (match) => match.replace("https://www.instagram.com", "https://www.ddinstagram.com")
+                );
+            }
+        });
+    },
+    onUnload: () => {
+        unpatch();
+    },
+    settings: Settings,
+}
---
+++ src/ui/pages/Settings.tsx
@@ -0,0 +1,22 @@
+import { Forms } from "@vendetta/ui/components";
+import { useProxy } from "@vendetta/storage";
+import { storage } from "@vendetta/plugin";
+
+const { FormSection, FormSwitch } = Forms;
+
+export default () => {
+    useProxy(storage);
+
+    return (
+        <FormSection title="Settings">
+            <FormSwitch 
+                label="Enable BetterInstagramEmbeds"
+                subLabel="Replace Instagram embeds with ddinstagram embeds"
+                value={storage.enabled}
+                onValueChange={(value) => {
+                    storage.enabled = value;
+                }}
+            />
+        </FormSection>
+    );
+};
