8070e866a40d3256d5b359b95cbbbc4e254595fa
GooglyBlox <cemcimyildiz@gmail.com>
2024-03-18 18:03:09 -0700

    add other socials

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -5,18 +5,49 @@ import { storage } from "@vendetta/plugin";
 const { FormSection, FormSwitch } = Forms;
 
 export default () => {
-    useProxy(storage);
+  useProxy(storage);
 
-    return (
-        <FormSection title="Settings">
-            <FormSwitch 
-                label="Enable BetterInstagramEmbeds"
-                subLabel="Replace Instagram embeds with ddinstagram embeds"
-                value={storage.enabled}
-                onValueChange={(value: any) => {
-                    storage.enabled = value;
-                }}
-            />
-        </FormSection>
-    );
+  storage.instagramEnabled ??= true;
+  storage.twitterEnabled ??= true;
+  storage.xEnabled ??= true;
+  storage.tiktokEnabled ??= true;
+
+  return (
+    <>
+      <FormSection title="Settings">
+        <FormSwitch
+          label="Enable BetterInstagramEmbeds"
+          subLabel="Replace Instagram embeds with ddinstagram embeds"
+          value={storage.instagramEnabled}
+          onValueChange={(value: boolean) => {
+            storage.instagramEnabled = value;
+          }}
+        />
+        <FormSwitch
+          label="Enable BetterTwitterEmbeds"
+          subLabel="Replace Twitter embeds with fxtwitter embeds"
+          value={storage.twitterEnabled}
+          onValueChange={(value: boolean) => {
+            storage.twitterEnabled = value;
+          }}
+        />
+        <FormSwitch
+          label="Enable BetterXEmbeds"
+          subLabel="Replace X embeds with fixupx embeds"
+          value={storage.xEnabled}
+          onValueChange={(value: boolean) => {
+            storage.xEnabled = value;
+          }}
+        />
+        <FormSwitch
+          label="Enable BetterTikTokEmbeds"
+          subLabel="Replace TikTok embeds with tnktok embeds"
+          value={storage.tiktokEnabled}
+          onValueChange={(value: boolean) => {
+            storage.tiktokEnabled = value;
+          }}
+        />
+      </FormSection>
+    </>
+  );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { findByProps } from "@vendetta/metro";
 import { before } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
 import Settings from "./Settings";
 
 const Messages = findByProps("sendMessage", "receiveMessage");
@@ -7,20 +8,53 @@ const Messages = findByProps("sendMessage", "receiveMessage");
 let unpatch;
 
 export default {
-    onLoad: () => {
-        unpatch = before("sendMessage", Messages, (args) => {
-            const content = args[1].content as string;
-            const instagramRegex = /https:\/\/www\.instagram\.com\/(p\/|reel\/)[^ ]+/g;
-            if (instagramRegex.test(content)) {
-                args[1].content = content.replace(
-                    instagramRegex,
-                    (match) => match.replace("https://www.instagram.com", "https://www.ddinstagram.com")
-                );
-            }
-        });
-    },
-    onUnload: () => {
-        unpatch();
-    },
-    settings: Settings,
-}
+  onLoad: () => {
+    unpatch = before("sendMessage", Messages, (args) => {
+      const content = args[1].content as string;
+
+      if (storage.instagramEnabled) {
+        const instagramRegex = /https:\/\/www\.instagram\.com\/(p\/|reel\/)[^ ]+/g;
+        if (instagramRegex.test(content)) {
+          args[1].content = content.replace(
+            instagramRegex,
+            (match) => match.replace("https://www.instagram.com", "https://www.ddinstagram.com")
+          );
+        }
+      }
+
+      if (storage.twitterEnabled) {
+        const twitterRegex = /https:\/\/twitter\.com\/[^ ]+/g;
+        if (twitterRegex.test(content)) {
+          args[1].content = content.replace(
+            twitterRegex,
+            (match) => match.replace("https://twitter.com", "https://fxtwitter.com")
+          );
+        }
+      }
+
+      if (storage.xEnabled) {
+        const xRegex = /https:\/\/x\.com\/[^ ]+/g;
+        if (xRegex.test(content)) {
+          args[1].content = content.replace(
+            xRegex,
+            (match) => match.replace("https://x.com", "https://fixupx.com")
+          );
+        }
+      }
+
+      if (storage.tiktokEnabled) {
+        const tiktokRegex = /https:\/\/(?:www\.)?tiktok\.com\/[^ ]+/g;
+        if (tiktokRegex.test(content)) {
+          args[1].content = content.replace(
+            tiktokRegex,
+            (match) => match.replace(/https:\/\/(?:www\.)?tiktok\.com/, "https://tnktok.com")
+          );
+        }
+      }
+    });
+  },
+  onUnload: () => {
+    unpatch();
+  },
+  settings: Settings,
+};
